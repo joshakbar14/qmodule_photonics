c484961203cb160b3125d1da385539e0314a9e2e
Your Name <jakabar14@outlook.com>
2023-02-22 22:47:38 +0000

    Added q factor calculation

--- src/FWHM_gaussianfit.py
+++ src/FWHM_gaussianfit.py
@@ -1,3 +1,7 @@
+# resource from https://stackoverflow.com/questions/10582795/finding-the-full-width-half-maximum-of-a-peak
+# adopted 22 February 2023
+# modified with qfactor formula Q = peak lambda / delta half peak width value
+
 import numpy as np
 import scipy.optimize as opt
 
@@ -5,8 +9,10 @@ def gauss(x, p): # p[0]==mean, p[1]==stdev
     return 1.0/(p[1]*np.sqrt(2*np.pi))*np.exp(-(x-p[0])**2/(2*p[1]**2))
 
 # Create some sample data
-known_param = np.array([2.0, .7])
-xmin,xmax = -1.0, 5.0
+#known_param = np.array([2.0, .7]) #mean 2.0 sd 0.7
+known_param = np.array([30.0, 3])
+#xmin,xmax = -1.0, 5.0
+xmin,xmax = 10, 110
 N = 1000
 X = np.linspace(xmin,xmax,N)
 Y = gauss(X, known_param)
@@ -17,7 +23,7 @@ Y += .10*np.random.random(N)
 # Renormalize to a proper PDF
 Y /= ((xmax-xmin)/N)*Y.sum()
 
-# Fit a guassian
+# Fit a gaussian
 p0 = [0,1] # Inital guess is a normal distribution
 errfunc = lambda p, x, y: gauss(x, p) - y # Distance to the target function
 p1, success = opt.leastsq(errfunc, p0[:], args=(X, Y))
@@ -26,6 +32,9 @@ fit_mu, fit_stdev = p1
 
 FWHM = 2*np.sqrt(2*np.log(2))*fit_stdev
 # print "FWHM", FWHM
+
+print(p1)
+print(FWHM)
 
 ## Plot data ##
 
--- src/FWHM_spline.py
+++ src/FWHM_spline.py
@@ -1,21 +1,31 @@
 # resource from https://stackoverflow.com/questions/10582795/finding-the-full-width-half-maximum-of-a-peak
-# copied 22 February 2023
+# adopted 22 February 2023
+# modified with qfactor formula Q = peak lambda / delta half peak width value
 
 import numpy as np
+import matplotlib.pyplot as plt
 from scipy.interpolate import UnivariateSpline
 
 def make_norm_dist(x, mean, sd):
     return 1.0/(sd*np.sqrt(2*np.pi))*np.exp(-(x - mean)**2/(2*sd**2))
 
-x = np.linspace(10, 110, 1000)
-green = make_norm_dist(x, 50, 10)
-pink = make_norm_dist(x, 60, 10)
+#x = np.linspace(10, 110, 1000)
+x = np.linspace(-1, 5, 1000)
+green = make_norm_dist(x, 30, 3)
+pink = make_norm_dist(x, 60, 4)
 
-blue = green + pink   
+blue = green + pink
+blue = green
 
 # create a spline of x and blue-np.max(blue)/2 
 spline = UnivariateSpline(x, blue-np.max(blue)/2, s=0)
 r1, r2 = spline.roots() # find the roots
+
+delta_hw = blue[int(r2)] - blue[int(r1)]
+peak = np.max(blue)
+
+qfactor = peak/delta_hw
+print(delta_hw, peak, qfactor)
 
 import pylab as pl
 pl.plot(x, blue)
