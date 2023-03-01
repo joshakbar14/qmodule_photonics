888459577fce1e69fff3a531f1604320f8da5777
Your Name <jakabar14@outlook.com>
2023-03-01 23:47:52 +0000

    edit gaussian fit and fourier transform

--- src/FWHM_gaussianfit.py
+++ src/FWHM_gaussianfit.py
@@ -8,38 +8,45 @@ import scipy.optimize as opt
 def gauss(x, p): # p[0]==mean, p[1]==stdev
     return 1.0/(p[1]*np.sqrt(2*np.pi))*np.exp(-(x-p[0])**2/(2*p[1]**2))
 
+## Sample Data ##
 # Create some sample data
-#known_param = np.array([2.0, .7]) #mean 2.0 sd 0.7
-known_param = np.array([30.0, 3])
-#xmin,xmax = -1.0, 5.0
-xmin,xmax = 10, 110
+known_param = np.array([1545, 2]) #mean 2.0 sd 0.7
+xmin,xmax = 1530, 1560
 N = 1000
 X = np.linspace(xmin,xmax,N)
 Y = gauss(X, known_param)
-
 # Add some noise
-Y += .10*np.random.random(N)
+Y += .30*np.random.random(N)
 
 # Renormalize to a proper PDF
-Y /= ((xmax-xmin)/N)*Y.sum()
+# Y /= ((xmax-xmin)/N)*Y.sum()
 
 # Fit a gaussian
-p0 = [0,1] # Inital guess is a normal distribution
+p0 = [0,1555] # Inital guess is a normal distribution
 errfunc = lambda p, x, y: gauss(x, p) - y # Distance to the target function
 p1, success = opt.leastsq(errfunc, p0[:], args=(X, Y))
 
 fit_mu, fit_stdev = p1
 
+#FWHM for Gaussian
 FWHM = 2*np.sqrt(2*np.log(2))*fit_stdev
 # print "FWHM", FWHM
 
+delta_hw = FWHM
+qfactor = fit_mu/delta_hw
+
+print(delta_hw, fit_mu, qfactor)
+print("peak", fit_mu,
+      "delta", delta_hw,
+      "qfactor", qfactor)
 print(p1)
-print(FWHM)
+print(FWHM/2)
 
 ## Plot data ##
 
 from pylab import *
 plot(X,Y)
-plot(X, gauss(X,p1),lw=3,alpha=.5, color='r')
+xlim(1530,1560)
+plot(X, gauss(X,p1)+max(Y)/4,lw=3,alpha=.5, color='r')
 axvspan(fit_mu-FWHM/2, fit_mu+FWHM/2, facecolor='g', alpha=0.5)
 show()
--- src/FWHM_spline.py
+++ src/FWHM_spline.py
@@ -9,22 +9,26 @@ from scipy.interpolate import UnivariateSpline
 def make_norm_dist(x, mean, sd):
     return 1.0/(sd*np.sqrt(2*np.pi))*np.exp(-(x - mean)**2/(2*sd**2))
 
-#x = np.linspace(10, 110, 1000)
-x = np.linspace(-1, 5, 1000)
+N = 1000
+x = np.linspace(10, 110, 1000)
+#x = np.linspace(-1, 5, 1000)
 green = make_norm_dist(x, 30, 3)
 pink = make_norm_dist(x, 60, 4)
 
 blue = green + pink
 blue = green
 
+#blue += .10*np.random.random(N)
+
 # create a spline of x and blue-np.max(blue)/2 
 spline = UnivariateSpline(x, blue-np.max(blue)/2, s=0)
 r1, r2 = spline.roots() # find the roots
 
-delta_hw = blue[int(r2)] - blue[int(r1)]
+delta_hw = r2 - r1
 peak = np.max(blue)
 
 qfactor = peak/delta_hw
+print(r1,r2)
 print(delta_hw, peak, qfactor)
 
 import pylab as pl
